9c8e46ac2a5605d749b701bfc310dc62a47b5c95
czahn24 <144357005+czahn24@users.noreply.github.com>
2023-09-18 14:39:00 -0500

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -15,6 +15,6 @@ for i in read_item_list:
 hit_ratio = hit_ratio/len(read_item_list)
 spacial_locality = counter(read_item_list)
 
-#temporal locality
+#temporal locality, this will probably change
 for i in read_item_list:
     print i.getAddr()
